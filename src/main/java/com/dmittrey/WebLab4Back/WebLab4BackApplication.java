6e69fa2bad96c8a0e1a72815a787c87fa5d31cb1
dmittrey <dmitryzubakhin@mail.ru>
2021-12-29 06:15:03 +0300

    Initial commit

---
+++ src/main/java/com/dmittrey/WebLab4Back/WebLab4BackApplication.java
@@ -0,0 +1,13 @@
+package com.dmittrey.WebLab4Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLab4BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLab4BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmittrey/WebLab4Back/WebLab4BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmittrey.WebLab4Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLab4BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
